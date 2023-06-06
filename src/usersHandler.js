dcee6a9b7eceeb97d7965d234f13106c025457a1
Yusuf <yusufisaabdullatif@gmail.com>
2023-06-06 13:31:39 +0700

    initial commit

--- src/usersHandler.js
+++ src/usersHandler.js
@@ -1,6 +1,5 @@
 const md5 = require('md5');
 const pool = require('./db');
-const bcrypt = require('bcrypt');
 
 const register = (req, res) => {
     const { username, email, password } = req.body;
