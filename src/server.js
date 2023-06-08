1d87b71679341a82daa7a995982731f3cbbf071f
Yusuf <yusufisaabdullatif@gmail.com>
2023-06-08 23:07:03 +0700

    initial commit

--- src/server.js
+++ src/server.js
@@ -2,15 +2,18 @@ const express = require('express');
 //const usersRoutes = require('./usersRoutes')
 //const foodRoutes = require('./eatRoutes')
 const app = express();
-const port = 3000
+//const port = 3000
 
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Hello world!");
+    res.send("Response success!");
 });
 
 //app.use("/users", usersRoutes);
 app.use(require("./eatRoutes"));
 
-app.listen(port, () => console.log(`App listening on port ${port}`));
+const server = app.listen(process.env.PORT || 3000, () => {
+    const port = server.address().port;
+    console.log(`App listening on port ${port}`)
+});
